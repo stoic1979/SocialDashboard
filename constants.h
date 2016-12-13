4db34e586cbfe008429f481db6c885159798489f
Navjot Singh <weavebytes@gmail.com>
2016-12-14 04:26:24 +0530

    introducing constants file

---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+//-----------------------------------------------
+//  Genral Constants
+//-----------------------------------------------
+#define USER_AGENT "SocialDashboard-Browser"
+
+#endif // CONSTANTS_H
